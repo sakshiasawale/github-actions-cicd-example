217ae602e2675a920cb3c01802bd8bc0754208fd
Sakshi Asawale <sakshiasawale3@gmail.com>
2026-02-05 18:29:15 +0530

    Test CI workflow on PR

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello CI/CD!'));
+app.get('/', (req, res) => res.send('Hello CI/CD Pipeline!'));
+
 
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -3,5 +3,5 @@ const request = require('supertest');
 
 test('GET / returns Hello CI/CD!', async () => {
   const response = await request(app).get('/');
-  expect(response.text).toBe('Hello CI/CD!');
+  expect(response.text).toBe('Hello CI/CD Pipeline!');
 });
